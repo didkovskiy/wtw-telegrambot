dcafa8eecd4f2d0abe60bba292ae9ac9c6330070
didkovskiy <didkovskiythebest@gmail.com>
2021-09-22 19:30:15 +0300

    WTWTB-0: skeleton Spring Boot project is created

---
+++ src/test/java/com/github/didkovskiy/wtwtelegrambot/WhatToWatchTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.didkovskiy.wtwtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToWatchTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
